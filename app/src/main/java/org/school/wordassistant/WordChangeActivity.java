a7b79b19e18d398839ca792463fe7a9f9bec5823
TheMostCai <1135279795@qq.com>
2020-12-15 10:05:45 +0800

    继续完善

--- app/src/main/java/org/school/wordassistant/WordChangeActivity.java
+++ app/src/main/java/org/school/wordassistant/WordChangeActivity.java
@@ -1,14 +1,14 @@
 package org.school.wordassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WordChangeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_word_change);
     }
-
 }
